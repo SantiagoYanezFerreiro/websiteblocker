8c0eed9a924285def16f131551a8c15263c25f44
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-04-26 22:30:19 +0200

    created main component and imported into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import WebsiteBlocker from "./components/WebsiteBlocker";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div>
+      <WebsiteBlocker />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/WebsiteBlocker.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WebsiteBlocker() {
+  return (
+    <div>
+      <h1>Website Blocker</h1>
+    </div>
+  );
+}
