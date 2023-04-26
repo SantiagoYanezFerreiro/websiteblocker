e0b4e941ae5a81f41ab41aff8e13522b1395163d
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-04-27 00:44:59 +0200

    button and state to add websites to block

--- src/components/WebsiteBlocker.jsx
+++ src/components/WebsiteBlocker.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function WebsiteBlocker() {
+  const [blockWebsites, setBlockedWebsites] = useState([]);
+
+  function addBlockedWebsite(website) {
+    if (!addBlockedWebsite.includes(website)) {
+      blockWebsites.push(website);
+    }
+  }
   return (
-    <div>
+    <div className="blocker-container">
       <h1>Website Blocker</h1>
+      <button onClick={addBlockedWebsite}>Add New Website</button>
     </div>
   );
 }
